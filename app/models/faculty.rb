5b151e250264eee8ab5796eae70e33d3fc8429a1
Rahul <rahul.k@simformsolutions.com>
2022-02-02 19:17:14 +0530

    Feat: Added a callbacks in student & faculty models.

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,21 +1,44 @@
 class Faculty < ApplicationRecord
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true
-    validates :birth_date, presence: true
-    validates :phone_number, length: { is: 10 }, numericality: true
-    validates :email, uniqueness: true
-    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, unless: Proc.new { |a| a.email.blank? }
-    validate :desgination_of_faculty
-    validates :birth_date, comparison: { less_than: Date.today, message: "can't be in future" }
-    def desgination_of_faculty
-      if (designation == "Ass. Prof" || designation == "Prof")
-      elsif (designation == "HOD" || designation == "Sr. Prof.")
-        errors.add(:designation, " must not be any from HOD and Sr. Prof.")
-      else
-        errors.add(:designation, "must  be any from Ass.Prof and Prof. ")
-      end
-    end
+  validates :first_name,:last_name, presence: true
+  validates :phone_number, format: {with: /\A[0-9]{10}\z/ }
+  validates :email, presence: true, uniqueness: true
+
+	after_validation do 
+		faculty.errors.add_to_email("Country can't be blank") if faculty.email.blank?
   end
-  
-  
+  validates :designation, exclusion: { in: %w(HOD Sr.Prof.) },inclusion: { in: %w(Ass.Prof. Prof) ,
+      message: "can't be %{value}" }
+  Date.today
+  validates :birth_date,comparison: {less_than_or_equal_to: Date.today,message:"birthday can't be in future"}
+  after_validation :check_birth_date
+
+  #ater_initialize callback
+  after_initialize do 
+      puts "Faculty object initialized"
+  end
+
+  #after_find callback will be called whenever active record loads the data
+  after_find do 
+      puts "Faculty Object found"
+  end
+
+  #after_create callback
+  after_create do 
+      puts "Faculty object created"
+  end
+
+  #after_update callback
+  after_update do |faculty|
+      puts "Faculty object updated"
+  end
+
+  #after_destroy callback (It will only be run when you use destroy)
+  after_destroy do |faculty|
+      puts "Faculty object destroyed"
+  end
+
+  private
+    def check_birth_date
+        puts "Date Of Birth is : #{self.birth_date}"
+    end
+end
